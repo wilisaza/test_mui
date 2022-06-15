612c920e97d313541be64b1007767f5856dc10a7
William Isaza <wilisaza@gmail.com>
2022-06-15 16:44:20 -0500

    Se crea barra de botones de herramientas y ventana de Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,29 +3,139 @@ import React from "react";
 import { styled, useTheme } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import SaveIcon from "@mui/icons-material/SaveOutlined";
+import ExitToAppIcon from "@mui/icons-material/ExitToAppOutlined";
+import SearchIcon from "@mui/icons-material/SearchOutlined";
+import QueryStatsIcon from "@mui/icons-material/QueryStatsOutlined";
+import SearchOffIcon from "@mui/icons-material/SearchOffOutlined";
+import AddIcon from "@mui/icons-material/AddOutlined";
+import RemoveIcon from "@mui/icons-material/RemoveOutlined";
+import Tooltip from "@mui/material/Tooltip";
+
+import Divider from "@mui/material/Divider";
+
+import FormBlock from "./FormBlock";
+
+let factorSize = 1;
+let factorDisplay = 1.4;
 
 function Form({ formStruct }) {
-  console.log(formStruct);
+  //console.log(formStruct);
+  let wHeight = parseInt(formStruct.Window["-Height"]);
+  let wWidth = parseInt(formStruct.Window["-Width"]);
   return (
-    <Box
-      sx={{
-        margin: "0 auto",
-        height: formStruct.Window["-Height"] + "px",
-        width: formStruct.Window["-Width"] + "px",
-        backgroundColor: "#e3dcdc",
-        border: "1px solid #565555",
-      }}
-    >
-      <AppBar position="static">
+    <Container>
+      <Box
+        sx={{
+          //margin: "0 auto",
+          height: wHeight * factorDisplay + 60 + "px",
+          //width: wWidth * factorDisplay + "px",
+          //height: "100%",
+          backgroundColor: "#e3dcdc",
+          border: "1px solid #565555",
+          borderRadius: 2,
+        }}
+      >
+        {/*<AppBar position="static">
         <Toolbar sx={{ minHeight: "100%" }}>
-          <Typography variant="caption" component="div" sx={{ flexGrow: 1 }}>
-            {formStruct.Window["-Title"]}
-          </Typography>
+          <IconButton color="inherit">
+            <SaveIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <ExitToAppIcon />
+          </IconButton>
         </Toolbar>
-      </AppBar>
-    </Box>
+    </AppBar>*/}
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            width: "fit-content",
+            border: (theme) => `1px solid ${theme.palette.divider}`,
+            borderRadius: 2,
+            bgcolor: "#c1c6ef",
+            color: "text.secondary",
+            "& svg": {
+              m: 1.5,
+            },
+            "& hr": {
+              mx: 0.5,
+            },
+          }}
+        >
+          <Tooltip title="Guardar">
+            <IconButton size="small" color="warning">
+              <SaveIcon fontSize="inherit" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Salir">
+            <IconButton size="small" color="secondary">
+              <ExitToAppIcon fontSize="inherit" />
+            </IconButton>
+          </Tooltip>
+          <Divider orientation="vertical" flexItem />
+          <Tooltip title="Introducir Consulta">
+            <IconButton size="small">
+              <SearchIcon fontSize="inherit" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Ejecutar Consulta">
+            <IconButton size="small">
+              <QueryStatsIcon fontSize="inherit" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Cancelar Consulta">
+            <IconButton size="small">
+              <SearchOffIcon fontSize="inherit" />
+            </IconButton>
+          </Tooltip>
+          <Divider orientation="vertical" flexItem />
+          <Tooltip title="Insertar Registro">
+            <IconButton size="small" color="success">
+              <AddIcon fontSize="inherit" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Eliminar Registro">
+            <IconButton size="small" color="error">
+              <RemoveIcon fontSize="inherit" />
+            </IconButton>
+          </Tooltip>
+        </Box>
+        <Box
+          sx={{
+            margin: "0 auto",
+            marginTop: "5px",
+            height: wHeight * factorSize + 60 + "px",
+            width: wWidth * factorSize + "px",
+            backgroundColor: "#adcbf9",
+            border: "1px solid #565555",
+            borderRadius: 1,
+          }}
+        >
+          {/*<AppBar position="static">
+            <Toolbar sx={{ minHeight: "100%" }}>
+              <Typography
+                variant="caption"
+                //component="div"
+                //sx={{ flexGrow: 1 }}
+              >
+                {formStruct.Window["-Title"]}
+              </Typography>
+            </Toolbar>
+        </AppBar>*/}
+          <Typography variant="caption">
+            <Box sx={{ backgroundColor: "#1976d2", color: "#ffffff" }}>
+              {formStruct.Window["-Title"]}
+            </Box>
+            <FormBlock />
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
---
+++ src/components/FormBlock.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FormBlock() {
+  return <div>FormBlock</div>;
+}
+
+export default FormBlock;
